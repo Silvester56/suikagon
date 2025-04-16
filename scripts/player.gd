7a73ee6adc0324100cffa565a845668d16f70682
Silvester56 <vargfoad@gmail.com>
2025-04-16 14:08:38 +0200

    Improve color and scale of polygons

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,13 +5,14 @@ extends CharacterBody2D
 var speed = 100
 var currentShape
 var canGetNextShape = true
+var rng = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	getNextShape()
 
 func _physics_process(delta: float) -> void:
 	var directionX := Input.get_axis("left", "right")
-	if Input.is_action_just_pressed("drop"):
+	if Input.is_action_just_pressed("drop") and !canGetNextShape:
 		drop()
 	if directionX:
 		velocity.x = directionX * speed
@@ -20,18 +21,19 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func drop() -> void:
-	canGetNextShape = true
-	currentShape.reparent($"..")
 	currentShape.freeze = false
 	currentShape.connect("body_entered", _on_shape_collision)
+	currentShape.reparent($"..")
+	canGetNextShape = true
 
 func _on_shape_collision(body) -> void:
+	currentShape.disconnect("body_entered", _on_shape_collision)
 	getNextShape()
 
 func getNextShape() -> void:
 	if canGetNextShape:
 		currentShape = Polygon.instantiate()
-		currentShape.setAttributes(3, 15)
+		currentShape.setAttributes(rng.randi_range(0, 4))
 		currentShape.freeze = true
 		call_deferred("add_child", currentShape)
 		canGetNextShape = false
--- scripts/polygon.gd
+++ scripts/polygon.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
-func setAttributes(edges: int, scale: float) -> void:
+var allEdges = [3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30]
+var allColors = [Color.SLATE_BLUE, Color.SALMON, Color.TOMATO, Color.REBECCA_PURPLE, Color.SPRING_GREEN, Color.LIGHT_STEEL_BLUE, Color.FOREST_GREEN, Color.AQUAMARINE, Color.PURPLE, Color.DARK_GOLDENROD, Color.CHARTREUSE]
+
+func setAttributes(index: int) -> void:
+	var edges = allEdges[index]
+	var radius = edges * (5 - index * 0.15)
 	var polygonVectorArray = []
 	var angle = 2 * PI / edges
 	for i in edges:
-		polygonVectorArray.push_back(Vector2(cos(angle * i) * scale, sin(angle * i) * scale))
+		polygonVectorArray.push_back(Vector2(cos(angle * i) * radius, sin(angle * i) * radius))
 	$CollisionShape2D.polygon = polygonVectorArray
 	$Polygon2D.polygon = polygonVectorArray
+	$Polygon2D.color = allColors[index]
