398c1f4bc7a8ad9f3596e60502ca71bd368ce3b1
Silvester56 <vargfoad@gmail.com>
2025-04-18 17:17:24 +0200

    Add confirmation screen before quitting

--- scripts/game.gd
+++ scripts/game.gd
@@ -42,3 +42,11 @@ func _on_retry_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_cancel_pressed() -> void:
+	get_tree().paused = false
+	$PauseScreen.hide()
+
+func _on_pause_pressed() -> void:
+	get_tree().paused = true
+	$PauseScreen.show()
